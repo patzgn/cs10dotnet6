caa8af1dafd39e7c0b60c18ba556a630a24ffa74
patzgn <103074854+patzgn@users.noreply.github.com>
2024-01-15 19:22:24 +0100

    Added Northwind SqlServer

--- PracticalApps/Northwind.Common.DataContext.Sqlite/NorthwindContextExtensions.cs
+++ PracticalApps/Northwind.Common.DataContext.Sqlite/NorthwindContextExtensions.cs
@@ -1,31 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Northwind.Common.DataContext.Sqlite
+namespace Shared;
+
+public static class NorthwindContextExtensions
 {
-    public static class NorthwindContextExtensions
+    /// <summary>
+    /// Adds NorthwindContext to the specified IServiceCollection. Uses the Sqlite database provider.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="relativePath">Set to override the default of ".."</param>
+    /// <returns>An IServiceCollection that can be used to add more services.</returns>
+    public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string relativePath = "..")
     {
-        /// <summary>
-        /// Adds NorthwindContext to the specified IServiceCollection. Uses the Sqlite database provider.
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="relativePath">Set to override the default of ".."</param>
-        /// <returns>An IServiceCollection that can be used to add more services.</returns>
-        public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string relativePath = "..")
-        {
-            string databasePath = Path.Combine(relativePath, "Northwind.db");
+        string databasePath = Path.Combine(relativePath, "Northwind.db");
 
-            services.AddDbContext<NorthwindContext>(options =>
-                options.UseSqlite($"Data Source={databasePath}")
-            );
+        services.AddDbContext<NorthwindContext>(options =>
+            options.UseSqlite($"Data Source={databasePath}")
+        );
 
-            return services;
-        }
+        return services;
     }
 }
